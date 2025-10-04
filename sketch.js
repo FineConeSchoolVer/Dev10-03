1362e8813c47071d87c83e9a47628352f918bedd
Wincci Ye <Wincciye@ocadu.ca>
2025-10-03 22:51:12 -0400

    playGifWhenShaking

--- sketch.js
+++ sketch.js
@@ -1,31 +1,77 @@
+let jumpingGif;
+let playbackSpeed = 1.0; 
+let backgroundColor;
+
+let speedMultiplier = 3.0; 
+let maxSpeed = 200.0;
+let minSpeedToPlay = 0.1;
+
+function preload() 
+{
+    // Load a GIF using loadImage
+    jumpingGif = loadImage('/IMG_2347.gif');
+}
+
 function setup() 
 {
-    // Show debug pane FIRST to cathch setup errors
-    showDebug(); 
+    // Show debug panel FIRST to catch setup errors
+    // showDebug();
+    
 
 	createCanvas(windowWidth, windowHeight);
+    backgroundColor = color(220, 211, 220);
 
     // Lock mobile gestures to prevent browser interference
-    lockGesture(); 
+    lockGestures(); 
+
+    textAlign(CENTER, CENTER);
 
     // Enable motion sensors with tap-to-start
     enableGyroTap('Tap to enable motion sensors');
 
     // Enable microphone with tap-to-start
-    enableMicTap('Tap to enable microphone');
+    // enableMicTap('Tap to enable microphone');
 }
 
 function draw()
 {
-    background(220);
+    background(backgroundColor);
 
     // Boolean: true when motion sensors are active (Status variables)
     if (window.sensorsEnabled) {
         
         // Use device rotation and acceleration
-        fill(255, 0, 0);
-        circle(width/2 + rotationY * 5, height/2 + rotationX * 5, 50);
+        // fill(255, 0, 0);
+        // circle(width/2 + rotationY * 5, height/2 + rotationX * 5, 50);
 
+        let tiltAmount = abs(rotationX);
+        playbackSpeed = tiltAmount * speedMultiplier;
+
+        debug(rotationX);
+        playbackSpeed = constrain(playbackSpeed, 0, maxSpeed);
+
+        if (playbackSpeed < minSpeedToPlay) {
+            jumpingGif.pause();
+        }
+        else {
+            jumpingGif.play();
+            jumpingGif.delay(int(100 / playbackSpeed));
+        }
+
+        push();
+        translate(width / 2, height / 2);
+        // rotate(HALF_PI);
+        imageMode(CENTER);
+        image(jumpingGif, 0, 0, height/4, width/4);
+        pop();
+
+    }
+
+    else {
+        fill(255, 100, 100);
+        text("Motion sensors not available", width/2, height/2);
+        text("On iOS: Tap to request motion permission", width/2, height/2 + 30);
+        text("Check device compatibility", width/2, height/2 + 60);
     }
 }
 
